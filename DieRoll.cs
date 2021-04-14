6bc4604e24d2d533d244f03e4171caea29e9467e
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-14 12:15:46 +0530

    modified die roll

---
+++ DieRoll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeNdLadder
+{
+    class DieRoll
+    {
+        public static void RollTheDie()
+        {
+            Random random = new Random();
+            int DieValue = random.Next(0, 6);
+            Console.WriteLine("player rolls a die" + DieValue);
+        }
+    }
+}
